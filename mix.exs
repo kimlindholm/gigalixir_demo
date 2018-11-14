b850a7487ce649d06a43e58c40070505b3808635
Kim Lindholm <misc@dynevision.com>
2018-11-14 18:16:42 +0700

    Update Elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule GigalixirDemo.MixProject do
     [
       app: :gigalixir_demo,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.7.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
