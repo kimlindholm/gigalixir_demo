e55b90c9c9b7adf6847db53145a0d3f93aab3a87
Kim Lindholm <misc@dynevision.com>
2018-11-14 16:55:32 +0700

    Add Distillery and run "mix release.init"

---
+++ rel/config.exs
@@ -0,0 +1,55 @@
+# Import all plugins from `rel/plugins`
+# They can then be used by adding `plugin MyPlugin` to
+# either an environment, or release definition, where
+# `MyPlugin` is the name of the plugin module.
+~w(rel plugins *.exs)
+|> Path.join()
+|> Path.wildcard()
+|> Enum.map(&Code.eval_file(&1))
+
+use Mix.Releases.Config,
+    # This sets the default release built by `mix release`
+    default_release: :default,
+    # This sets the default environment used by `mix release`
+    default_environment: Mix.env()
+
+# For a full list of config options for both releases
+# and environments, visit https://hexdocs.pm/distillery/config/distillery.html
+
+
+# You may define one or more environments in this file,
+# an environment's settings will override those of a release
+# when building in that environment, this combination of release
+# and environment configuration is called a profile
+
+environment :dev do
+  # If you are running Phoenix, you should make sure that
+  # server: true is set and the code reloader is disabled,
+  # even in dev mode.
+  # It is recommended that you build with MIX_ENV=prod and pass
+  # the --env flag to Distillery explicitly if you want to use
+  # dev mode.
+  set dev_mode: true
+  set include_erts: false
+  set cookie: :"o)3J)J&X@r%g.[`qn[G68Z(1*kA$!0!0yr]?w$4zJv%dida:sF*mQs=@Wq3`70|n"
+end
+
+environment :prod do
+  set include_erts: true
+  set include_src: false
+  set cookie: :"o>QEIIC(lLV;afK,6!1z3!;w1bA>.QX)5I5,_C;C@?_.bbIIV{Bf2>6%0srzo6~7"
+  set vm_args: "rel/vm.args"
+end
+
+# You may define one or more releases in this file.
+# If you have not set a default release, or selected one
+# when running `mix release`, the first release in the file
+# will be used by default
+
+release :gigalixir_demo do
+  set version: current_version(:gigalixir_demo)
+  set applications: [
+    :runtime_tools
+  ]
+end
+
